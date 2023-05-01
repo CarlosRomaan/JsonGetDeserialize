f67f970b8bb13e10d9fae781e74c2cbbc8a41471
JuanCarlos <JuanCarlos@JUANCARLOS-PC>
2023-05-01 01:29:15 -0700

    Agregar archivos de proyecto.

---
+++ JsonGetDeserialize/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Text.Json;
+using JsonGetDeserialize.Models;
+
+namespace JsonGetDeserialize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/todos/1";
+            string getResponse = Get(url);
+            Data data = JsonSerializer.Deserialize<Data>(getResponse);
+            
+            Console.WriteLine(data.Completed);
+            
+        }
+
+        static string Get(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            return reader.ReadToEnd().Trim();
+        }
+    }
+}
